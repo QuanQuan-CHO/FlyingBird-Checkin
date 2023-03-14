854e4252318712a10df54f01fa7a93865168155b
WLQ <836200779@qq.com>
2023-03-15 00:40:46 +0800

    Update datetime format

--- index.py
+++ index.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     hosts = [line.strip() for line in open('hosts.txt', 'r', encoding='utf-8').readlines()]
     for host in hosts:
         try:
-            log(datetime.date.today())
+            log(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             login(host)
             log(f'login {host}')
             log(checkin(host)+'\n')
