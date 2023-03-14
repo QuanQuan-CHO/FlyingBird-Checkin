3d3069b3c8475b1731c1693f0a73f374f20f1975
WLQ <836200779@qq.com>
2023-03-14 13:56:16 +0800

    Add log file

--- index.py
+++ index.py
@@ -6,15 +6,16 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-# 账号
 email = ''
-# 密码
 passwd = ''
-# session
 session = requests.session()
 
 
-# 登陆
+def log(msg):
+    print(msg)
+    os.system(f'echo "{msg}">> log')
+
+
 def login(host):
     url = f'{host}/auth/login'
     params = {
@@ -34,36 +35,25 @@ def login(host):
     }
     res = session.post(url=url, headers=headers, data=params, timeout=10)
     msg = res.json()['msg']
-    print(msg)
-    ret = ''
     if msg == '登录成功':
-        # 登陆成功之后，去更新hosts.txt
+        # 登陆成功之后，更新hosts.txt
         del headers['Content-Type']
         html = session.get(f'{host}/user', headers=headers).text
         soup = BeautifulSoup(html, 'lxml')
-        hosts = set()
+        hosts = []
         for i in soup.find_all('h5'):
-            a = i.find('a', text=re.compile('http.*'))
-            if a:
-                hosts.add(a.text)
+            host = i.find('a', text=re.compile('http.*'))
+            if host:
+                hosts.append(host.text)
         with open('hosts.txt', 'w', encoding='utf-8') as f:
             for i in hosts:
                 f.write(f'{i}\n')
-        # 获取登陆信息
-        statistics = soup.find_all(class_='card card-statistic-2')
-        for i in statistics:
-            for j in i.text.split('\n'):
-                if len(j) > 1:
-                    a = j.replace('\n', '').replace('\r', '').replace('升级套餐', '').strip()
-                    ret += a + ' '
-            ret += '\n\n'
-        return ret
     else:
         raise Exception(msg)
 
 
 # 速鹰666签到领流量
-def clockIn(host):
+def checkin(host) -> str:
     url = f'{host}/user/checkin'
     headers = {
         'Connection': 'keep-alive',
@@ -75,30 +65,22 @@ def clockIn(host):
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9',
     }
-    res = session.post(url=url, headers=headers)
-    json = res.json()
-    print(json)
-    return json
-
-
-def main_handler():
-    hosts = [i.strip() for i in open('hosts.txt', 'r', encoding='utf-8').readlines()]
-    for host in hosts:
-        try:
-            print('try', host)
-            lmsg = login(host)
-            json = clockIn(host)
-            msg = json['msg']
-            print(lmsg + '今日签到 ' + msg)
-        except Exception as e:
-            print(e)
+    response = session.post(url=url, headers=headers)
+    return response.json()['msg']
 
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     print(datetime.datetime.now())
     email, passwd = sys.argv[1:]
-    if (not len(email)) or (not len(passwd)):
-        print('email or passwd is null')
-        exit()
-    main_handler()
+
+    hosts = [line.strip() for line in open('hosts.txt', 'r', encoding='utf-8').readlines()]
+    for host in hosts:
+        try:
+            log(datetime.date.today())
+            login(host)
+            log(f'login {host}')
+            log(checkin(host))
+            break
+        except Exception as e:
+            print(e)
