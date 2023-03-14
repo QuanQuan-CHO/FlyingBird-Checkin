57b7770c4609c6a5781509b5c0b078c7b62e5e07
WLQ <836200779@qq.com>
2023-03-14 23:22:24 +0800

    Delete headers

--- index.py
+++ index.py
@@ -21,24 +21,12 @@ def login(host):
     params = {
         'email': email,
         'passwd': passwd,
-        'code': ''
     }
-    headers = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Origin': url,
-        'Referer': f'{url}/auth/login',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-        'X-Requested-With': 'XMLHttpRequest',
-    }
-    res = session.post(url=url, headers=headers, data=params, timeout=10)
+    res = session.post(url=url, data=params, timeout=10)
     msg = res.json()['msg']
     if msg == '登录成功':
         # 登陆成功之后，更新hosts.txt
-        del headers['Content-Type']
-        html = session.get(f'{host}/user', headers=headers).text
+        html = session.get(f'{host}/user').text
         soup = BeautifulSoup(html, 'lxml')
         hosts = []
         for i in soup.find_all('h5'):
@@ -52,20 +40,9 @@ def login(host):
         raise Exception(msg)
 
 
-# 速鹰666签到领流量
 def checkin(host) -> str:
     url = f'{host}/user/checkin'
-    headers = {
-        'Connection': 'keep-alive',
-        'Content-Length': '0',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Origin': url,
-        'Referer': f'{host}/user',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-    }
-    response = session.post(url=url, headers=headers)
+    response = session.post(url=url)
     return response.json()['msg']
 
 
@@ -78,7 +55,7 @@ if __name__ == '__main__':
             log(datetime.date.today())
             login(host)
             log(f'login {host}')
-            log(checkin(host))
+            log(checkin(host)+'\n')
             break
         except Exception as e:
             log(e)
